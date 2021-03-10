df531e7da46c261d358232488b574e4c84e0be7f
craftbeersci <arudolph.bio@gmail.com>
2021-03-09 21:57:22 -0500

    Finished updating the specific gravity functions

--- src/predicted_value_functions.R
+++ src/predicted_value_functions.R
@@ -2,8 +2,9 @@
 # gravity, pH, and cells in suspension
 
 ###################
-### Specific Gravity Functions
+########## Specific Gravity Functions
 
+#####
 # predicted value for the log function from equation 1 in Reid et al. 2021
 log_func <- function(log_par, tvec){
   pe <- log_par[1] # lower asymtote (i.e. final gravity, FG)
@@ -13,6 +14,7 @@ log_func <- function(log_par, tvec){
   log_pred <- pe + (pi-pe)/(1+exp(-b(tvec-m)))
 }
 
+#####
 # predicted value for the generalized logistic function (eq. 2 in Reid et al. 2021)
 gen_log_func <- lfunction(gen_log_par, tvec){
   pe <- gen_log_par[1] # lower asymtote (i.e. final gravity, FG)
@@ -24,21 +26,7 @@ gen_log_func <- lfunction(gen_log_par, tvec){
   # TODO check out this s parameter, I don't know if it's actually multiplied or what?
 }
 
-# predicted value from the Apparent Degree of Fermentation (ADF)
-# From the paper: "The difficulty with the use of this method as reported is that the
-# Original Gravity or P0 must be fixed rather than be free to allow the best fit. Practically, P0
-# is set to the largest density value in the fermentation dataset. This procedure is analogous
-# to assuming or fixing an intercept in linear regression, limiting one’s ability to accurately
-# predict the change in the dependent variable ADFt"
-
-adf_func <- function(log_par, tvec){
-  p0 <- adf_par[1] # lower asymtote (i.e. final gravity, FG)
-  pi <- adf_par[2] # higher asymtote (i.e. original gravity, OG) 
-  m <- adf_par[3] # inflection point of curve
-  b <- adf_par[4] # rate of decrease at inflection point (max rate of decrease)
-  log_pred <- pe + (pi-pe)/(1+exp(-b(tvec-m)))
-}
-
+#####
 # predicted values for the exponential (decay) function
 exp_func <- function(exp_par, tvec){
   n0 <- exp_par[1] # OG of fermentation
@@ -54,7 +42,6 @@ exp_func <- function(exp_par, tvec){
 
 ###################
 # Functions for pH
-
 
 
 
