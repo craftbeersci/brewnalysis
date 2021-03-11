28475501a46a67dbd45bf2e6ccbde01c01348010
craftbeersci <arudolph.bio@gmail.com>
2021-03-11 10:08:08 -0500

    have the parameter fitting output rss as well

--- src/estimate_parameters.R
+++ src/estimate_parameters.R
@@ -6,28 +6,25 @@
 #####
 # Estimate parameters for the log function from equation 1 in Reid et al. 2021
 est_log_pars <- function(log_guess, tvec, obs){
-  log_pars <- optim(par = log_guess, fn = log_rss, method = "Nelder-Mead", tvec = tvec, obs = obs)
-  log_pars <- log_pars$par
+  log_fit <- optim(par = log_guess, fn = log_rss, method = "Nelder-Mead", tvec = tvec, obs = obs)
+  log_pars <- c(log_fit$par,log_fit$value)
   return(list(log_pars))
-  # return(log_pars)
 }
 
 #####
 # Estimate parameters for the generalized logistic function (eq. 2 in Reid et al. 2021)
 est_gen_log_pars <-function(gen_log_guess, tvec, obs){
-  log_pars <- optim(par = gen_log_guess, fn = gen_log_rss, method = "Nelder-Mead", tvec = tvec, obs = obs)
-  log_pars <- log_pars$par
-  return(list(log_pars))
-  # return(log_pars)
+  gen_log_fit <- optim(par = gen_log_guess, fn = gen_log_rss, method = "Nelder-Mead", tvec = tvec, obs = obs)
+  gen_log_pars <- c(gen_log_fit$par, gen_log_fit$value)
+  return(list(gen_log_pars))
 }
 
 #####
 # Estimate parameters for the exponential (decay) function
 est_exp_pars <- function(exp_guess, tvec, obs){
-  exp_pars <- optim(par = exp_guess, fn = exp_rss, method = "Nelder-Mead", tvec = tvec, obs = obs)
-  exp_pars <- exp_pars$par
+  exp_fit <- optim(par = exp_guess, fn = exp_rss, method = "Nelder-Mead", tvec = tvec, obs = obs)
+  exp_pars <- c(exp_fit$par, exp_fit$value)
   return(list(exp_pars))
-  # return(exp_pars)
 }
 
 ###################
